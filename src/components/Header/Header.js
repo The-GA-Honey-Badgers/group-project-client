87f0ae503bfa03582b713faa8c37041947750b95
Sean-Drew <SDrew989@gmail.com>
2020-06-02 16:37:43 -0400

    Style navbar & buttons
    Styled navbar.
    Changed body background color.
    Styled navbar buttons consistently (sign up, sign in, change password,
    sign out, home).
    Styled form field submit buttons consistently.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,42 @@
 import React, { Fragment } from 'react'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
+import { Nav, Navbar, Button } from 'react-bootstrap'
+
+const navbarTextStyle = {
+  color: 'white'
+}
 
 const authenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#change-password">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
-    <Nav.Link href="#post-create">Create Post</Nav.Link>
+    <Nav.Link href="#post-create"><Button className='nav-btn'>Create Post</Button></Nav.Link>
+    <Nav.Link href="#change-password"><Button className='nav-btn'>Change Password</Button></Nav.Link>
+    <Nav.Link href="#sign-out"><Button className='nav-btn'>Sign Out</Button></Nav.Link>
   </Fragment>
 )
 
 const unauthenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#sign-up">Sign Up</Nav.Link>
-    <Nav.Link href="#sign-in">Sign In</Nav.Link>
+    <Nav.Link href="#sign-up"><Button className='nav-btn'>Sign Up</Button></Nav.Link>
+    <Nav.Link href="#sign-in"><Button className='nav-btn'>Sign In</Button></Nav.Link>
   </Fragment>
 )
 
 const alwaysOptions = (
   <Fragment>
-    <Nav.Link href="/">Home</Nav.Link>
+    <Nav.Link to="/"><Button className='nav-btn'>Home</Button></Nav.Link>
   </Fragment>
 )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
+  <Navbar className='navbar' variant="dark" expand="md">
     <Navbar.Brand href="#">
       honey-badgers-client
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
-        { alwaysOptions }
         { user ? authenticatedOptions : unauthenticatedOptions }
+        { alwaysOptions }
+        { user && <span style={navbarTextStyle} className="navbar-text mr-2">Welcome, {user.email}</span>}
       </Nav>
     </Navbar.Collapse>
   </Navbar>
